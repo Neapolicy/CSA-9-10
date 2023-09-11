0cc7443b3847ef1729fb57540c49756a48e0778e
Matthew Lin <144387789+Neapolicy@users.noreply.github.com>
2023-09-11 15:09:00 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,11 @@ public class Main
 {
   public static void main(String[]arge)
   {
-    System.out.println("Hello, world.");
-    System.out.print("This took way longer than needed.");
+    int i = 17;
+    final double n = 3.10;
+    boolean b = true;
+    System.out.println(i);
+    System.out.println(n);
+    System.out.print(b);
   }
 }
