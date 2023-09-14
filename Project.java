21457e69c115805c93386d0c9b9f144872ab189f
Matthew Lin <144387789+Neapolicy@users.noreply.github.com>
2023-09-14 15:32:40 -0400

    Add files via upload

--- Project.java
+++ Project.java
@@ -23,23 +23,31 @@ public class Project
       int numberOfTails = 0;
       if (rand_int == 1){
         numberOfHeads += 1;
-      }else{
-        numberOfTails += 1;
+        System.out.println("heads!");
+        if (call.equals("heads")){
+          System.out.println("correct!");
+        }
+        else{
+          System.out.println("unlucky!");
+        }
       }
+      if (rand_int == 0){
+        numberOfHeads += 1;
+        System.out.println("tails!");
+        if (call.equals("tails")){
+          System.out.println("correct!");
+        }
+        else{
+          System.out.println("unlucky!");
+        }
+      }
+
 
       final double probabilityOfHeads = 0.5;
       boolean isHeads = true;
       System.out.println("The probability of a fair coin landing on heads is " + probabilityOfHeads + ".");
       System.out.println("The coin has landed heads " + numberOfHeads + " times.");
       System.out.println();
-
-      System.out.println("I will now flip the coin.");
-      isHeads = false;
-      System.out.println();
-      System.out.println("I will now flip the coin again.");
-      isHeads = true;
-      System.out.println();
-      System.out.println("The coin has landed heads " + numberOfHeads + " time.");
       double proportionOfTails = (int)(numberOfTails/(numberOfTails + numberOfHeads)); //compound expression
       System.out.println("The coin has landed tails " + proportionOfTails * 100 + " percent of the time.");
     }
