f12fd7a86867b163c5783bbaf63f1a8ba7866c99
Matthew Lin <144387789+Neapolicy@users.noreply.github.com>
2023-09-19 15:36:20 -0400

    Update Project.java

--- Project.java
+++ Project.java
@@ -4,56 +4,23 @@ public class Project
 {
     public static void main(String[] args)
     {
-        System.out.println("Do you like to gamble?");
+        System.out.println("Do you want a rigged coin flip or fair one?");
         Scanner s = new Scanner(System.in);
         String res = s.nextLine();
-        if (res.equals("yes")){
-            System.out.println("good.");
-        }else{
-            System.out.println("WELL TOO BAD.");
+        if (res.equals("rigged"))
+        {
+            System.out.println("Coins will be rigged.");
+        }
+        else
+        {
+            System.out.println("Coins will be fair.");
         }
         System.out.println("Heads or tails?");
-        String ans = s.nextLine();
-        System.out.println(ans + ", eh?");
+        String answer = s.nextLine();
         s.close();
-        System.out.println();
-        Random rand = new Random();
-        int rand_int = rand.nextInt(2);
-        int numberOfHeads = 0;
-        int numberOfTails = 0;
-        if (rand_int == 1){
-            ++numberOfHeads;
-            System.out.println("heads!");
-            if (ans.equals("heads")){
-                System.out.println("correct!");
-            }
-            else{
-                System.out.println("unlucky!");
-            }
-        }
-        if (rand_int == 0){
-            numberOfHeads += 1;
-            System.out.println("tails!");
-            if (ans.equals("tails")){
-                System.out.println("correct!");
-            }
-            else{
-                System.out.println("unlucky!");
-            }
-        }
+        Coin coin = new Coin();
+        coin.rigged_flip();
 
 
-        final double probabilityOfHeads = 0.5;
-        System.out.println("The probability of a fair coin landing on heads is " + probabilityOfHeads + ".");
-        System.out.println("The coin has landed heads " + numberOfHeads + " times.");
-        System.out.println();
-        double proportionOfTails =(numberOfTails/(numberOfTails + numberOfHeads)); //compound expression
-        System.out.println("The coin has landed tails " + proportionOfTails * 100 + " percent of the time.");
-
-        // checklist dump because I don't know how to incorporate it neatly
-        boolean boole = true;
-        PLACEHOLDER stuff = new PLACEHOLDER();
-        stuff.toss();
-        stuff.examine();
     }
 }
